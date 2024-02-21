4afafd421770367daefb50247134a1e15ac07cac
999lama <71217614+999lama@users.noreply.github.com>
2024-02-21 22:41:27 +0300

    Add websocket and handle the connection

---
+++ TechincalAssmeent/TechincalAssmeent/Module/Model/WebSocketViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  WebSocketViewModel.swift
+//  TechincalAssmeent
+//
+//  Created by Lama Albadri on 21/02/2024.
+//
+
+import Foundation
+import Combine
+
+class SendMessageViewModel: ObservableObject {
+    @Published var recivesMessages: [String] = []
+    @Published var isAlertPresented: Bool = false
+
+    private var webSocketTask: URLSessionWebSocketTask?
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() { }
+    
+    func createWebSocketConnection() {
+        guard let url = URL(string: "wss://echo.websocket.org") else {
+            return
+        }
+
+        webSocketTask = URLSession.shared.webSocketTask(with: url)
+        webSocketTask?.resume()
+
+        receiveMessages()
+    }
+
+    func disconnectWebSocket() {
+           webSocketTask?.cancel(with: .goingAway, reason: nil)
+           cancellables.removeAll() // Remove all cancellables when disconnecting
+       }
+
+    
+    func sendMessage(_ messageToSend: String) {
+        let message = URLSessionWebSocketTask.Message.string(messageToSend)
+        webSocketTask?.send(message) { error in
+            if let error = error {
+                print("WebSocket send error: \(error)")
+            } else {
+                DispatchQueue.main.async {
+                    self.isAlertPresented = true
+                }
+            }
+        }
+    }
+
+
+
+    func receiveMessages() {
+        webSocketTask?.receive { result in
+            switch result {
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    Just(text)
+                        .receive(on: DispatchQueue.main)
+                        .sink { [weak self] receivedText in
+                            self?.recivesMessages.append(receivedText)
+                        }
+                        .store(in: &self.cancellables)
+                case .data(let data):
+                    print("Received data: \(data)")
+                @unknown default:
+                    break
+                }
+
+                // Continue listening for more messages
+                self.receiveMessages()
+
+            case .failure(let error):
+                print("WebSocket receive error: \(error)")
+            }
+        }
+    }
+    
+
+}
